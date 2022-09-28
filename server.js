39813d978ee575b44dc22a76b9397fe8782beafe
Fenil6122 <fkdpatel6511@gmail.com>
2022-09-27 22:51:12 -0400

    commit

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 
 // setup a 'route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.redirect("/about");
+    res.redirect("/views/about");
 });
 
 // setup http server to listen on HTTP_PORT
