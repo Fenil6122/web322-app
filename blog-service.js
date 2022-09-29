1666b9ff72ce978427b55034f51f2ee504ddcd1f
Fenil6122 <fkdpatel6511@gmail.com>
2022-09-29 15:41:18 -0400

    commit

---
+++ blog-service.js
@@ -0,0 +1,56 @@
+const fs = require("fs"); // required at the top of your module
+const path = require("path");
+
+let posts = [];
+let categories = [];
+
+module.exports = {
+    initialize,
+    getAllPosts,
+    getPublishedPosts,
+    getCategories
+}
+
+function initialize () {
+    return new Promise((resolve, reject) => {
+        let result = "";
+        fs.readFile(path.join(__dirname, "data/posts.json"), 'utf8', (err, data) => {
+            if (err) {
+                result = "unable to read file";
+            };
+            posts = JSON.parse(data);
+        });
+        fs.readFile(path.join(__dirname, "data/categories.json"), 'utf8', (err, data) => {
+            if (err) {
+                result = "unable to read file";
+            }
+            categories = JSON.parse(data);
+        });
+
+        if(result.length) {
+            reject(result);
+        } 
+    });
+}
+
+function getAllPosts() {
+    return new Promise((resolve, reject) => {
+        if(posts.length) resolve(posts)
+        else reject("no results returned");
+    })
+}
+
+function getPublishedPosts() {
+    return new Promise((resolve, reject) => {
+        let publishedPosts = posts.filter(post => post.published === true);
+        if(publishedPosts.length) resolve(publishedPosts)
+        else reject("no results returned");
+    })
+}
+
+function getCategories() {
+    return new Promise((resolve, reject) => {
+        if(categories.length) resolve(categories)
+        else reject("no results returned");
+    })
+}
--- server.js
+++ server.js
@@ -1,11 +1,46 @@
-var HTTP_PORT = process.env.PORT || 8080;
-var express = require("express");
-var app = express();
+/*********************************************************************************
+ *  WEB322 – Assignment 02
+ *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source
+ *  (including 3rd party web sites) or distributed to other students.
+ *
+ *  Name: Fenil KetanKumar Patel
+ *  Student ID: 134516210
+ *  Date: 30th Sept,2022
+ *
+ *  Online (Cyclic) Link: 
+ *
+ ********************************************************************************/
 
-// setup a 'route' to listen on the default url path
-app.get("/", (req, res) => {
-    res.redirect('/about');
-});
-
-// setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT);
+ const express = require('express');
+ const path = require('path');
+ const service = require('./blog-service');
+ 
+ const app = express();
+ 
+ const HTTP_PORT = process.env.port || 8080;
+ 
+ app.use(express.static('public'));
+ 
+ app.get('/', (req, res) => {
+     res.redirect('/about');
+ })
+ 
+ app.get('/about', (req, res) => {
+     res.sendFile(path.join(__dirname, 'views/about.html'));
+ })
+ 
+ app.get('/blog', (req, res) => {
+     service.getPublishedPosts().then(data => res.json(data)).catch(err => res.json(err));
+ })
+ 
+ app.get('/posts', (req, res) => {
+     service.getAllPosts().then(data => res.json(data)).catch(err => res.json(err));
+ })
+ 
+ app.get('/categories', (req, res) => {
+     service.getCategories().then(data => res.json(data)).catch(err => res.json(err));
+ })
+ 
+ app.listen(HTTP_PORT, () => {
+     service.initialize().then((data) => console.log(data)).catch((err) => console.log(err));
+ })
